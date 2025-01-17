fe630ee0677636aef83f4218004ba2cce338a249
Agathon006 <agaton615@gmail.com>
2025-01-17 11:12:32 +0300

    feat: Configured lint-staged and prettier in pre-commit

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from "react";
-import "./Button.css";
+import React, { FC } from 'react';
+import './Button.css';
 
-type ButtonSize = "small" | "medium" | "big";
-type ButtonVariant = "text" | "contained" | "outlined";
+type ButtonSize = 'small' | 'medium' | 'big';
+type ButtonVariant = 'text' | 'contained' | 'outlined';
 
 export type ButtonProps = {
   children?: React.ReactNode;
@@ -12,26 +12,26 @@ export type ButtonProps = {
 
 const Button: FC<ButtonProps> = ({
   children,
-  size = "medium",
-  variant = "contained",
+  size = 'medium',
+  variant = 'contained',
   ...props
 }) => {
-  const rootClasses = ["ccl-button"];
+  const rootClasses = ['ccl-button'];
 
-  if (size === "small") {
-    rootClasses.push("ccl-button-small");
-  } else if (size === "big") {
-    rootClasses.push("ccl-button-big");
+  if (size === 'small') {
+    rootClasses.push('ccl-button-small');
+  } else if (size === 'big') {
+    rootClasses.push('ccl-button-big');
   }
 
-  if (variant === "text") {
-    rootClasses.push("ccl-button-text");
-  } else if (variant === "outlined") {
-    rootClasses.push("ccl-button-outlined");
+  if (variant === 'text') {
+    rootClasses.push('ccl-button-text');
+  } else if (variant === 'outlined') {
+    rootClasses.push('ccl-button-outlined');
   }
 
   return (
-    <button {...props} className={rootClasses.join(" ")}>
+    <button {...props} className={rootClasses.join(' ')}>
       {children}
     </button>
   );
