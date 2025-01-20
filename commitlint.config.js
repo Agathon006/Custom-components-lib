5dd0b5d670cb6c063a0a1dadafe58d9b11bf69d9
Agathon006 <agaton615@gmail.com>
2025-01-20 21:36:19 +0300

    chore(text-field): extra changes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,23 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [
-        2,
-        'always',
-        [
-          'feat',
-          'refactor',
-          'fix',
-          'chore',
-          'docs',
-          'style',
-          'ci',
-          'test',
-          'perf',
-          'revert',
-          'build',
-          'vercel',
-        ],
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'refactor',
+        'fix',
+        'chore',
+        'docs',
+        'style',
+        'ci',
+        'test',
+        'perf',
+        'revert',
+        'build',
+        'vercel',
       ],
-    },
-  };
+    ],
+  },
+};
