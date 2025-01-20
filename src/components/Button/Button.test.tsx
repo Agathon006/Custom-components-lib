b7f3a8b621aeca3aab5f703c5092ddbea77f9865
Agathon006 <agaton615@gmail.com>
2025-01-20 12:08:57 +0300

    refactor(app): stable version before css modules feature

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { Button, ButtonProps } from '../components/Button';
+import { Button, ButtonProps } from '.';
 import '@testing-library/jest-dom';
 
 describe('Button', () => {
