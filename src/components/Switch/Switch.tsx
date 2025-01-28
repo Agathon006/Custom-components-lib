77319410373ae33c043c4edcbef6cf8f5bd2f186
Agathon006 <agaton615@gmail.com>
2025-01-28 13:17:19 +0300

    feat(component): label prop

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -3,15 +3,26 @@ import classes from './Switch.module.scss';
 import { clsx } from '../../utils';
 
 export type SwitchProps = {
+  label?: string;
   type?: 'checkbox';
   className?: string;
 } & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'>;
 
-export const Switch: FC<SwitchProps> = ({ type = 'checkbox', className, ...props }) => {
+export const Switch: FC<SwitchProps> = ({
+  label,
+  type = 'checkbox',
+  className,
+  ...props
+}) => {
+
   return (
-    <label className={clsx(className, classes.switch_wrapper)} data-testid="switch-wrapper">
-      <input {...props} type={type} className={classes.switch_checkbox} role="checkbox" />
+    <label
+      className={clsx(className, classes.switch_wrapper)}
+      data-testid="switch-wrapper"
+    >
+      <input {...props} type={type} className={classes.switch_checkbox} role='checkbox' />
       <div className={classes.switch_slider}></div>
+      {label && <span className={classes.switch_label_text}>{label}</span>}
     </label>
   );
 };
