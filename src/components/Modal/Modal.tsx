99c36d1a89baad404656e41ccb701729b27002a2
Agathon006 <agaton615@gmail.com>
2025-01-29 11:00:06 +0300

    refactor(component): proper ModalProps type + onClose instead of ()=>onClose?.()

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,15 +5,13 @@ import { clsx } from '../../utils';
 export type ModalProps = {
   open?: boolean;
   onClose?: () => void;
-  children?: React.ReactNode;
-  className?: string;
 } & React.ComponentPropsWithoutRef<'div'>;
 
 export const Modal: FC<ModalProps> = ({ open, onClose, children, className, ...props }) => {
   return (
     <>
       <div
-        onClick={() => onClose?.()}
+        onClick={onClose}
         className={clsx(classes.modal_overlay, open && classes.modal_overlay_open)}
       />
       <div
